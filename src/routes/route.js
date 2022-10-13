3b3b1439f3e3b3a057bbe611d046068e943287f5
abhi587 <abhishek.pawan14@gmail.com>
2022-10-13 16:49:34 +0530

    done

---
+++ src/routes/route.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const router = express.Router()
+const UserController = require('../controllers/userController')
+const Auth = require('../middleWares/auth')
+const ProductController = require('../controllers/productController')
+
+
+//test-api
+router.get('/test-me', function(req, res) {
+    res.send({ status: true, message: "test-api working fine" })
+})
+
+
+//*********************************USER API************************************************** */
+router.post('/register', UserController.userRegistration)
+router.post('/login', UserController.userLogin)
+router.get('/user/:userId/profile', Auth.authentication, UserController.profileDetails)
+router.put('/user/:userId/profile', Auth.authentication, UserController.userProfileUpdate)
+
+
+//*********************************PRODUCT API**************************************************** */
+router.post('/products', ProductController.registerProduct)
+router.put('/products/:productId', ProductController.updateProductDetails)
+
+
+router.all("/*", function (req, res) {
+    res
+        .status(400)
+        .send({ status: false, message: "invalid http request" });
+});
+
+module.exports = router
